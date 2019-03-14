5349949ea25cdab6afc004fef84039a5e143968a
klinok <slesha92@yandex.ru>
2019-03-14 16:18:26 +0300

    налаживаем автозагрузку

---
+++ src/Vikonapi.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     Vikonapi
+ * @author      nfpi
+ * @link        https://db-nica.ru/
+ * @copyright   Copyright(c) 2019
+ * @version     0.1.0
+ **/
+
+namespace Nfpi\Vikonapi;
+
+use Curl\Curl;
+
+class Vikonapi
+{
+    public function __construct()
+    {
+        $curl = new Curl;
+        $curl->get('https://db-nica.ru/');
+        if ($curl->error) {
+            echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+        } else {
+            echo 'Response:' . "\n";
+            var_dump($curl->response);
+        }
+    }
+}
