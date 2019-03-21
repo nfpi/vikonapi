65d7d90975777f72e9f379c9b37f642f3428b524
Alexey <slesha92@yandex.ru>
2019-03-21 09:51:51 +0300

    добавлены методы get put post delete

--- src/Vikonapi/Request.php
+++ src/Vikonapi/Request.php
@@ -4,7 +4,7 @@
  * @author      nfpi
  * @link        https://db-nica.ru/
  * @copyright   Copyright(c) 2019
- * @version     0.1.0
+ * @version     1.0.0
  **/
 
 namespace Nfpi\Vikonapi;
@@ -13,19 +13,76 @@ use Curl\Curl;
 
 class Request
 {
-    public function __construct()
+    private $_domenApi = 'https://db-nica.ru/';
+    private $_apiKey;
+    private $_version = '1';
+
+    public function __construct($apiKey = '')
     {
+        $this->_apiKey = $apiKey;
     }
 
-    public function get()
+    public function get($method, $item = '')
     {
         $curl = new Curl;
-        $curl->get('https://db-nica.ru/');
+        $curl->setHeader('Authorization', 'Bearer ' . $this->_apiKey);
+        $curl->get($this->_domenApi . 'api/v' . $this->_version . '/' . $method . '/' . $item);
         if ($curl->error) {
-            echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+            return array(
+                'success' => false,
+                'error' => $curl->errorCode . ': ' . $curl->errorMessage,
+                'description' => isset($curl->response->error) ? $curl->response->error : '',
+            );
         } else {
-            echo 'Response:' . "\n";
-            var_dump($curl->response);
+            return $curl->response;
+        }
+    }
+
+    public function put($method, $parameters)
+    {
+        $curl = new Curl();
+        $curl->setHeader('Authorization', 'Bearer ' . $this->_apiKey);
+        $curl->put($this->_domenApi . 'api/v' . $this->_version . '/' . $method, $parameters);
+        if ($curl->error) {
+            return array(
+                'success' => false,
+                'error' => $curl->errorCode . ': ' . $curl->errorMessage,
+                'description' => isset($curl->response->error) ? $curl->response->error : '',
+            );
+        } else {
+            return $curl->response;
+        }
+    }
+
+    public function post($method, $item, $parameters)
+    {
+        $curl = new Curl();
+        $curl->setHeader('Authorization', 'Bearer ' . $this->_apiKey);
+        $curl->post($this->_domenApi . 'api/v' . $this->_version . '/' . $method . '/' . $item, $parameters);
+        if ($curl->error) {
+            return array(
+                'success' => false,
+                'error' => $curl->errorCode . ': ' . $curl->errorMessage,
+                'description' => isset($curl->response->error) ? $curl->response->error : '',
+            );
+        } else {
+            return $curl->response;
+        }
+    }
+
+    public function delete($method, $item)
+    {
+        $curl = new Curl();
+        $curl->setHeader('Authorization', 'Bearer ' . $this->_apiKey);
+        $curl->delete($this->_domenApi . 'api/v' . $this->_version . '/' . $method . '/' . $item);
+        if ($curl->error) {
+            return array(
+                'success' => false,
+                'error' => $curl->errorCode . ': ' . $curl->errorMessage,
+                'description' => isset($curl->response->error) ? $curl->response->error : '',
+            );
+        } else {
+            return $curl->response;
         }
     }
 }
